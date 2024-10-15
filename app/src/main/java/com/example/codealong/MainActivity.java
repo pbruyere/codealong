d7a30276163f40b9769dde60996e1c9694a6a641
pbruyere <93731136+pbruyere@users.noreply.github.com>
2024-10-15 14:03:59 -0400

    buttons+firebase
    linked firebase to app
    added two differently customized buttons on main activity

--- app/src/main/java/com/example/codealong/MainActivity.java
+++ app/src/main/java/com/example/codealong/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.codealong;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button m_btnRegister;
+    private Button m_btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        m_btnRegister = findViewById(R.id.btnRegister);
+        m_btnLogin = findViewById(R.id.btnLogin);
     }
 }
